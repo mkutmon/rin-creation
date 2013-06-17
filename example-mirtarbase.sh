5a32abe9f5fff242c189dcb1a39f8ca31a84797b
mkutmon <mkutmon@gmail.com>
2013-06-17 20:53:40 +0200

    example script for mirtarbase

---
+++ example-mirtarbase.sh
@@ -0,0 +1,2 @@
+java -cp RINCreator/dist/conversion.jar cytargetlinker.conversion.MirTarBase -i hsa_MTI.csv --bridgeDbFile Hs_Derby_20120602.bridge mirbase-v19.bridge -o mirtarbase-hsa-3.5.xgmml --organism "Homo sapiens" -v "3.5"
+
